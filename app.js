4caeed1fcef8222c296860ca625dbeec9ee307a1
Harshika <pinnintiharshika@gmail.com>
2021-10-27 17:32:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var indexRouter = require('./routes/mydata');
+var mydata = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/mydata', mydataRouter);
+app.use('/mydata', mydata);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
